37f66d52df20e030e9bf45229684b7bd155d142d
MatthewLZR <mlopez@re7systems.com>
2022-01-10 10:19:49 +0100

    totes les dependecies auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,12 @@ import { GameModule } from './game/game.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { configService } from './game/config/config.service';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [GameModule, TypeOrmModule.forRoot( 
     configService.getTypeOrmConfig(),
-  ), UserModule],
+  ), UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
