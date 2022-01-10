9412a72a1a3d3bff98cbfaa3004ce3a25ff16a83
MatthewLZR <mlopez@re7systems.com>
2022-01-10 11:08:12 +0100

    jwt-password

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { configService } from './game/config/config.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 
+
 @Module({
   imports: [GameModule, TypeOrmModule.forRoot( 
     configService.getTypeOrmConfig(),
