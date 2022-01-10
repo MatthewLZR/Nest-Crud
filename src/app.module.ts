78ada8520ac0f57a75920a101e37c7d269889851
MatthewLZR <mlopez@re7systems.com>
2022-01-10 10:16:18 +0100

    auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,12 @@ import { AppService } from './app.service';
 import { GameModule } from './game/game.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { configService } from './game/config/config.service';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [GameModule, TypeOrmModule.forRoot( 
     configService.getTypeOrmConfig(),
-  )],
+  ), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
